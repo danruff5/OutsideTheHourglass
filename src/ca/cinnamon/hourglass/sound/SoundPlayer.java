9fd8836aaae245f5dc198d8a075ab1f9a3008916
Fishback <b_fishback@hotmail.com>
2017-02-08 13:41:24 -0500

    Update SoundPlayer.java
    Changed the method it looks for files, as the previous method didn't seem to like path names that changed directories. Should work now.

--- src/ca/cinnamon/hourglass/sound/SoundPlayer.java
+++ src/ca/cinnamon/hourglass/sound/SoundPlayer.java
@@ -13,11 +13,10 @@ import javax.sound.sampled.Clip;
  */
 
 public enum SoundPlayer {
-			STEP("Sounds/stepstone_1.wav"),
-			SWORD("Sounds/sword sound.wav"),
-			WALL("Sounds/metal.wav");
+			STEP("../Sounds/stepstone_1.wav"),
+			SWORD("../Sounds/sword sound.wav"),
+			WALL("../Sounds/metal.wav");
 
-	
     public static enum Volume {
         MUTE, LOW, MEDIUM, HIGH;
     } // Volume;
@@ -28,10 +27,10 @@ public enum SoundPlayer {
     // Constructor to construct each element of the enum with its own sound file.
     SoundPlayer(String fileName) {
         try {
-            // Use URL (instead of File) to read from disk and JAR.
-            URL url = this.getClass().getClassLoader().getResource(fileName);
-            // Set up an audio input stream piped from the sound file.
-            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(url);
+           // URL url = this.getClass().getClassLoader().getResource(fileName);
+           // AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(url);
+              File soundFile = new File(fileName);
+              AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(soundFile);
             // Get a clip resource.
             clip = AudioSystem.getClip();
             // Open audio clip and load samples from the audio input stream.
