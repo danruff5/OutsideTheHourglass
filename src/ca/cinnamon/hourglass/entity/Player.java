5ffdeb4721eab9c62275a710cf35ba7c88ba5de9
Fishback <b_fishback@hotmail.com>
2017-03-07 13:39:12 -0500

    Override methods
    Override some methods so the wall collision sound only plays for the player.

--- src/ca/cinnamon/hourglass/entity/Player.java
+++ src/ca/cinnamon/hourglass/entity/Player.java
@@ -8,6 +8,7 @@ package ca.cinnamon.hourglass.entity;
 import ca.cinnamon.hourglass.entity.mob.Mob;
 import ca.cinnamon.hourglass.gui.Keys;
 import ca.cinnamon.hourglass.map.Map;
+import ca.cinnamon.hourglass.map.tile.Tile;
 import ca.cinnamon.hourglass.screen.Screen;
 import ca.cinnamon.hourglass.sound.SoundPlayer;
 
@@ -19,42 +20,123 @@ import com.sun.glass.events.KeyEvent;
  *
  * @author Daniel
  */
-public class Player extends Mob {
-    private Keys keys;
-    
-    public int speedLimit=150;
-    public Player(Keys keys,Point spawn) {
-        super(spawn);
+public class Player extends Mob
+{
+	private Keys keys;
 
-        this.keys = keys;
-        this.ATK=2;
-    }
-    
-    public void Tick() {
-        if (keys.keys[KeyEvent.VK_UP].pressed) {
-            // Player Move Up.
-            SoundPlayer.STEP.play();
-            this.moveUp(currentMap.tiles[loc.x][loc.y-1]);
-        }
-        if (keys.keys[KeyEvent.VK_RIGHT].pressed) {
-            // Player Move Right.
-            SoundPlayer.STEP.play();
-            this.moveRight(currentMap.tiles[loc.x+1][loc.y]);
-        }
-        if (keys.keys[KeyEvent.VK_DOWN].pressed) {
-            // Player Move Down.
-            SoundPlayer.STEP.play();
-            this.moveDown(currentMap.tiles[loc.x][loc.y+1]);
-        }
-        if (keys.keys[KeyEvent.VK_LEFT].pressed) {
-            // Player Move Left.
-            SoundPlayer.STEP.play();
-            this.moveLeft(currentMap.tiles[loc.x-1][loc.y]);
-        }
-    }
-    @Override
-    public void Draw(Screen screen) {
-    	screen.blit(sprites.add("./Pictures/wizzard.bmp"), this.loc.x*Map.tileWidth, (this.loc.y-0)*Map.tileWidth,Map.tileWidth,Map.tileHeight);
-    	
-    }
+	public int speedLimit = 150;
+
+	public Player(Keys keys, Point spawn)
+	{
+		super(spawn);
+
+		this.keys = keys;
+		this.ATK = 2;
+	}
+
+	public void Tick()
+	{
+		if (keys.keys[KeyEvent.VK_UP].pressed)
+		{
+			// Player Move Up.
+			SoundPlayer.STEP.play();
+			this.moveUp(currentMap.tiles[loc.x][loc.y - 1]);
+		}
+		if (keys.keys[KeyEvent.VK_RIGHT].pressed)
+		{
+			// Player Move Right.
+			SoundPlayer.STEP.play();
+			this.moveRight(currentMap.tiles[loc.x + 1][loc.y]);
+		}
+		if (keys.keys[KeyEvent.VK_DOWN].pressed)
+		{
+			// Player Move Down.
+			SoundPlayer.STEP.play();
+			this.moveDown(currentMap.tiles[loc.x][loc.y + 1]);
+		}
+		if (keys.keys[KeyEvent.VK_LEFT].pressed)
+		{
+			// Player Move Left.
+			SoundPlayer.STEP.play();
+			this.moveLeft(currentMap.tiles[loc.x - 1][loc.y]);
+		}
+	}
+
+	@Override
+	public void moveUp(Tile T)
+	{
+		if (!T.collide(this))
+		{
+			currentMap.changedTile.add(new Point(this.loc));
+			this.loc.y -= 1;
+		}
+		else
+		{
+			SoundPlayer.WALL.play();
+		}
+	}
+
+	@Override
+	public void moveDown(Tile T)
+	{
+		if (!T.collide(this))
+		{
+			currentMap.changedTile.add(new Point(this.loc));
+			this.loc.y += 1;
+		}
+		else
+		{
+			SoundPlayer.WALL.play();
+		}
+	}
+
+	@Override
+	public void moveRight(Tile T)
+	{
+		if (!T.collide(this))
+		{
+			currentMap.changedTile.add(new Point(this.loc));
+			this.loc.x += 1;
+		}
+		else
+		{
+			SoundPlayer.WALL.play();
+		}
+	}
+
+	@Override
+	public void moveLeft(Tile T)
+	{
+		if (!T.collide(this))
+		{
+			currentMap.changedTile.add(new Point(this.loc));
+			this.loc.x -= 1;
+		}
+		else
+		{
+			SoundPlayer.WALL.play();
+		}
+	}
+
+	@Override
+	public int Attack(Entity E)
+	{
+		SoundPlayer.SWORD.play();
+		return E.Hurt(ATK);
+	}
+
+	@Override
+	public int Hurt(int DAM)
+	{
+		HP -= DAM;
+		return HP;
+	}
+
+	@Override
+	public void Draw(Screen screen)
+	{
+		screen.blit(sprites.add("./Pictures/wizzard.bmp"), this.loc.x * Map.tileWidth, (this.loc.y - 0) * Map.tileWidth,
+				Map.tileWidth, Map.tileHeight);
+
+	}
 }
