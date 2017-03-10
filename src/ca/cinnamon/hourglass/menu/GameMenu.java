d5b47a0f0bbe89e673f10dd625edfc8e5f5a32d8
Fishback <b_fishback@hotmail.com>
2017-03-10 17:21:56 -0500

    Re-added player attack code

--- src/ca/cinnamon/hourglass/menu/GameMenu.java
+++ src/ca/cinnamon/hourglass/menu/GameMenu.java
@@ -17,72 +17,83 @@ import java.util.Iterator;
  *
  * @author dckra
  */
-public class GameMenu extends Menu {
+public class GameMenu extends Menu
+{
 
-    private Player player;
-    private Keys keys;
-    
-    
-    public GameMenu(int screen_width, int screen_height, Keys keys) {
-        this.keys = keys;
-        
-        // init
-        player = new Player(keys, Map.currentMap.GetRandomFloorTile());
-        player.currentMap = Map.currentMap;
-        Map.player = player;
-        Map.currentMap.entities.add(player);
-    } // GameMenu(int, int);
+	private Player player;
+	private Keys keys;
 
-    @Override
-    public void init(MenuManager.MenuType id) {
-        
-    } // init(MenuType);
+	public GameMenu(int screen_width, int screen_height, Keys keys)
+	{
+		this.keys = keys;
 
-    @Override
-    public void draw(Screen screen) {
-        Map.currentMap.draw(screen);
-    } // draw(Screen);
+		// init
+		player = new Player(keys, Map.currentMap.GetRandomFloorTile());
+		player.currentMap = Map.currentMap;
+		Map.player = player;
+		Map.currentMap.entities.add(player);
+	} // GameMenu(int, int);
 
-    @Override
-    public void tick() {
-        
-        
+	@Override
+	public void init(MenuManager.MenuType id)
+	{
 
-        Map.player.Tick();
-        Iterator<Entity> itr = Map.currentMap.entities.iterator();
-        while (itr.hasNext()) {
-            Entity e = itr.next();
-            if (e.getLocation().equals(Map.player.getLocation()) && !e.equals(Map.player)) {
-                if (Map.player.Attack(e) < 1) {
-                    itr.remove();
-                    ++Map.player.score;
-                }
-            }
-        }
+	} // init(MenuType);
 
-        //player.Tick();
-        for (Entity e : Map.currentMap.entities) {
-            if (e != Map.player) {
-                e.Tick();
-            }
-        }
-        for (Entity e : Map.currentMap.entities) {
-            if (e.getLocation().equals(Map.player.getLocation()) && !e.equals(Map.player)) {
-                e.Attack(Map.player);
-            }
+	@Override
+	public void draw(Screen screen)
+	{
+		Map.currentMap.draw(screen);
+	} // draw(Screen);
 
-        }
-        itr = Map.currentMap.entities.iterator();
-        while (itr.hasNext()) {
-            try {
-                Mob e = (Mob) itr.next();
-                if (e.HP <= 0 && e != Map.player) {
-                    itr.remove();
-                    ++Map.player.score;
-                }
-            } catch (Exception ex) {
-            }
-        }
-    } // tick();
+	@Override
+	public void tick()
+	{
+
+		Map.player.Tick();
+		Iterator<Entity> itr = Map.currentMap.entities.iterator();
+		while (itr.hasNext())
+		{
+			Entity e = itr.next();
+			// checks if player is "attacking", and has an entity in range
+			if (Map.player.State() == 1 && Map.player.HitDetection(e) && !e.equals(Map.player))
+			{
+				if (Map.player.Attack(e) < 1)
+				{
+					Map.player.score += e.Death();
+					itr.remove();
+				}
+			}
+		}
+		// Puts player back in 'idle' state
+		Map.player.ResetState();
+
+		// player.Tick();
+		for (Entity e : Map.currentMap.entities)
+		{
+			if (e != Map.player) e.Tick();
+		}
+		for (Entity e : Map.currentMap.entities)
+		{
+			if (e.getLocation().equals(Map.player.getLocation()) && !e.equals(Map.player)) e.Attack(Map.player);
+
+		}
+		itr = Map.currentMap.entities.iterator();
+		while (itr.hasNext())
+		{
+			try
+			{
+				Mob e = (Mob) itr.next();
+				if (e.HP <= 0 && e != Map.player)
+				{
+					Map.player.score += e.Death();
+					itr.remove();
+				}
+			}
+			catch (Exception ex)
+			{
+			}
+		}
+	} // tick();
 
 } // GameMenu;
