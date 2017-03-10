ba55eeb35b7fa05c06c1ccfa82268c9bf17d30dd
Fishback <b_fishback@hotmail.com>
2017-03-10 16:34:49 -0500

    Re-added player attack code, removed old version

--- src/ca/cinnamon/hourglass/gui/MainComponent.java
+++ src/ca/cinnamon/hourglass/gui/MainComponent.java
@@ -198,16 +198,21 @@ public class MainComponent extends Canvas implements Runnable {
 	   
 	   Map.player.Tick();
 	   Iterator<Entity> itr = Map.currentMap.entities.iterator();
-	   while (itr.hasNext())
-	   {
-	   	Entity e=itr.next();
-	   	if(e.getLocation().equals(Map.player.getLocation())&&!e.equals(Map.player))
-	   		if (Map.player.Attack(e)<1)
-	   		{
-	   			itr.remove();
-	   			++Map.player.score;
-	   		}
-	   }
+     while (itr.hasNext())
+     {
+     	Entity e=itr.next();
+     	//checks if player is "attacking", and has an entity in range
+       if (Map.player.State() == 1 && Map.player.HitDetection(e) &&!e.equals(Map.player))
+       {
+       		if (Map.player.Attack(e)<1)
+       		{
+       			Map.player.score += e.Death();
+       			itr.remove();
+       		}
+       }
+     }
+     //Puts player back in 'idle' state
+     Map.player.ResetState();
 	   
 	   //player.Tick();
 	   for (Entity e : Map.currentMap.entities) {
@@ -227,12 +232,14 @@ public class MainComponent extends Canvas implements Runnable {
 	       	Mob e=(Mob)itr.next();
 	       	if(e.HP<=0&&e!=Map.player)
 	       	{
+	       		Map.player.score += e.Death();
 	       		itr.remove();
-	       		++Map.player.score;
 	       	}
 	   	}
 	   	catch(Exception ex){}
 	   }
+
+
 	   /*
 	   // TODO: This loop needs to be redone becasue of removing the entities...
 	   for (int i = 0; i < Map.currentMap.entities.size(); ++i) {
